ede61c9708ea24964ce230732d1dbf230768a79c
Prechak <benedict.precha@gmail.com>
2024-05-09 12:16:27 +0700

    (ex-1 and ex2) : add new code

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firtName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurant = "Chef's Table by Chef James";
+const billTotal = 15000;
+const restaurantTotal = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+billTotal = 5000;
+restaurantTotal = 1500;
